db17d5c48ea5e8e7d749c00a0d74d6c30c7b359c
jacobkim9881 <jacobkim9881@gmail.com>
2021-05-30 12:55:58 +0900

    feat: changed array number for each language selection

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,13 +3,16 @@ import React, { Component } from 'react';
 import { Main, Texts, Head4 } from '../../Css'
 import info from '../api/info.config.json'
 
+const lang = localStorage.getItem('NationLang');
+const idx = lang === null || parseInt(lang) === 0 ? 0 : 1;
+
 class About extends Component {
     render() {
         return (
             <Main>
                 <Texts>
-        <Head4>{info.page.about.head[1]}</Head4>
-                    {info.page.about.body[1]}
+        <Head4>{info.page.about.head[idx]}</Head4>
+                    {info.page.about.body[idx]}
                 <br/>
                 {/*
                 Part time job for washing utensils in a hotel at Itaewon(2019.11)<br/>
@@ -20,14 +23,14 @@ class About extends Component {
                 Security guard at a bank(2016.08 - 2016.10) <br/>
                 Part time job for taking stocks(2016.03 - 2016.06) <br/>
                 */}
-                <a href={info.page.about.resumeKUrl}>{info.page.about.resumeK[1]}</a><br/>
-            <a href={info.page.about.resumeUrl}>{info.page.about.resumeE[1]}</a><br/>
+                <a href={info.page.about.resumeKUrl}>{info.page.about.resumeK[idx]}</a><br/>
+            <a href={info.page.about.resumeUrl}>{info.page.about.resumeE[idx]}</a><br/>
                 <br/>
-            <Head4>{info.page.about.posts[1]}</Head4>
-            <a href="https://dev.to/jacobkim9881/running-node-server-with-mysql-on-my-smartphone-1eb3">{info.page.about.postAdd1[1]}</a><br />
-            <a href="https://dev.to/jacobkim9881/hosting-web-server-at-cafe24-getting-a-job-in-korea-f67" >{info.page.about.postAdd2[1]}</a> <br/>               
-            <a href="https://github.com/jacobkim9881/myexetensiontutorial" >{info.page.about.postAdd3[1]}</a> <br/>               
-            <a href="https://github.com/jacobkim9881/shoppingweb" >{info.page.about.postAdd4[1]}</a>                
+            <Head4>{info.page.about.posts[idx]}</Head4>
+            <a href="https://dev.to/jacobkim9881/running-node-server-with-mysql-on-my-smartphone-1eb3">{info.page.about.postAdd1[idx]}</a><br />
+            <a href="https://dev.to/jacobkim9881/hosting-web-server-at-cafe24-getting-a-job-in-korea-f67" >{info.page.about.postAdd2[idx]}</a> <br/>               
+            <a href="https://github.com/jacobkim9881/myexetensiontutorial" >{info.page.about.postAdd3[idx]}</a> <br/>               
+            <a href="https://github.com/jacobkim9881/shoppingweb" >{info.page.about.postAdd4[idx]}</a>                
  
                 </Texts>
             </Main>
--- src/components/pages/portfolio/Portfolio.js
+++ src/components/pages/portfolio/Portfolio.js
@@ -3,6 +3,9 @@ import { Main, Texts, Head4 } from '../../../Css'
 import styled from 'styled-components'
 import info from '../../api/info.config.json'
 
+const lang = localStorage.getItem('NationLang');
+const idx = lang === null || parseInt(lang) === 0 ? 0 : 1;
+
 class Portfolio extends Component {
     render() {
         return (
@@ -10,41 +13,41 @@ class Portfolio extends Component {
                 <Texts>
                 <Head4>Portfolios</Head4>
                 <a href="https://codepen.io/jacobkim9881/pen/OJPabvL">
-		{info.page.portfolio.pfolioAdd1[1]}
+		{info.page.portfolio.pfolioAdd1[idx]}
 		</a><br />
-		- {info.page.portfolio.pfolioAdd1X[1]}
+		- {info.page.portfolio.pfolioAdd1X[idx]}
 <br/>
 <a href="https://ltmaker.netlify.app">
-		{info.page.portfolio.pfolioAdd2[1]}
+		{info.page.portfolio.pfolioAdd2[idx]}
 		</a><br />
-		- {info.page.portfolio.pfolioAdd2X[1]}
+		- {info.page.portfolio.pfolioAdd2X[idx]}
 		<br />
                 <div>
 		    <a href="https://chrome.google.com/webstore/detail/mouse-footprint-tailing/bgmbliffojkhembdalfdgldpeihhpdnm?hl=ko">
-		{info.page.portfolio.pfolioAdd3[1]}
+		{info.page.portfolio.pfolioAdd3[idx]}
 		</a><br/>
-		- {info.page.portfolio.pfolioAdd3X[1]}
+		- {info.page.portfolio.pfolioAdd3X[idx]}
 		<br/>
 		<br/>
 
                     <a href="https://jacobkim9881.github.io/shoppingweb">
-		{info.page.portfolio.pfolioAdd4[1]}
+		{info.page.portfolio.pfolioAdd4[idx]}
 		</a><br/>
                     <a href="https://github.com/jacobkim9881/shoppingweb">
 		Git repository
 		</a>
                     <p>
                     Frontend <br />
-                    - {info.page.portfolio.pfolioAdd4X1[1]}
+                    - {info.page.portfolio.pfolioAdd4X1[idx]}
 		<br />
-                    - {info.page.portfolio.pfolioAdd4X2[1]}
+                    - {info.page.portfolio.pfolioAdd4X2[idx]}
 		<br />
-                    - {info.page.portfolio.pfolioAdd4X3[1]}
+                    - {info.page.portfolio.pfolioAdd4X3[idx]}
 		<br />
                     Backend <br />
-                    - {info.page.portfolio.pfolioAdd4X4[1]}
+                    - {info.page.portfolio.pfolioAdd4X4[idx]}
 		<br />
-                    - {info.page.portfolio.pfolioAdd4X5[1]}
+                    - {info.page.portfolio.pfolioAdd4X5[idx]}
 		<br />
                     </p>
                 </div>
@@ -52,19 +55,19 @@ class Portfolio extends Component {
 		YouTubes
 		</Head4>
                     <a href="https://www.youtube.com/playlist?list=PL_xbjJpRyD5Nxsjhvd3enwi69MZ0cGNl_">
-		{info.page.portfolio.pfolioAdd5[1]}
+		{info.page.portfolio.pfolioAdd5[idx]}
 		</a><br />
-                    - {info.page.portfolio.pfolioAdd5X[1]}
+                    - {info.page.portfolio.pfolioAdd5X[idx]}
 		<br />
                     <a href="https://www.youtube.com/watch?v=SUYgcdyObYU">
-		{info.page.portfolio.pfolioAdd6[1]}
+		{info.page.portfolio.pfolioAdd6[idx]}
 		</a><br/>
-                    - {info.page.portfolio.pfolioAdd6X[1]}
+                    - {info.page.portfolio.pfolioAdd6X[idx]}
 		<br />
                     <a href="https://youtu.be/0B4sTW3O6_A">
-		{info.page.portfolio.pfolioAdd7[1]}
+		{info.page.portfolio.pfolioAdd7[idx]}
 		</a><br />
-                    - {info.page.portfolio.pfolioAdd7X[1]}
+                    - {info.page.portfolio.pfolioAdd7X[idx]}
                     </Texts>
             </Main>
         );
